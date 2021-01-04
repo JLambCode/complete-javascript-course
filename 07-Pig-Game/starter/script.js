76dc27cd3858c6894eeedf0e6b60aa138b0c6886
SaveJL101 <66347648+SaveJL101@users.noreply.github.com>
2021-01-04 17:12:25 -0500

    Update script.js

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -19,25 +19,52 @@ diceEl.classList.add('hidden');
 const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true;
 
-//dice function
-btnRoll.addEventListener('click', function () {
-  const dice = Math.trunc(Math.random() * 6) + 1;
-
-  diceEl.classList.remove('hidden');
-
-  diceEl.src = `dice-${dice}.png`;
-
-  if (dice !== 1) {
-    currentScore += dice;
-    document.getElementById(
-      `current--${activePlayer}`
-    ).textContent = currentScore;
-  } else {
+const togglePlayer = function(){
     document.getElementById(`current--${activePlayer}`).textContent = 0;
     activePlayer = activePlayer === 0 ? 1 : 0;
     currentScore = 0;
     player0El.classList.toggle('player--active');
     player1El.classList.toggle('player--active');
-  }
+};
+
+//dice function
+btnRoll.addEventListener('click', function () {
+    if(playing){
+        const dice = Math.trunc(Math.random() * 6) + 1;
+
+        diceEl.classList.remove('hidden');
+      
+        diceEl.src = `dice-${dice}.png`;
+      
+        if (dice !== 1) {
+          currentScore += dice;
+          document.getElementById(
+            `current--${activePlayer}`
+          ).textContent = currentScore;
+        } else {
+          togglePlayer();
+        }
+    }
+});
+
+btnHold.addEventListener('click', function(){
+    if(playing){
+        scores[activePlayer] += currentScore;
+
+        document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+    
+        if(scores[activePlayer] >= 100){
+            playing = false;
+            document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+            document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+        }
+    
+        togglePlayer();
+    }
+});
+
+btnNew.addEventListener('click', function(){
+    playing = true;
 });
