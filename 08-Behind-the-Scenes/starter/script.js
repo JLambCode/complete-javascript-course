0d96d31d64c5af6ff8baa7de73214230c2a05f4a
SaveJL101 <66347648+SaveJL101@users.noreply.github.com>
2021-01-06 08:12:07 -0500

    Update script.js

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+console.log(me);
+
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991;
+
+console.log(addDecl(2, 3));
+//console.log(addExpr(2, 3));
+//console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+const addArrow = (a, b) => a + b;
