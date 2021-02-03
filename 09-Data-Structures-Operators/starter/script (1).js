3f34149f7610c0694f363dcd126a723fe00b01bc
JLambCode <66347648+SaveJL101@users.noreply.github.com>
2021-02-03 15:01:55 -0500

    Create script (1).js

---
+++ 09-Data-Structures-Operators/starter/script (1).js
@@ -0,0 +1,148 @@
+'use strict';
+
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours,
+
+  order: function (startIndex, mainIndex) {
+    return [this.starterMenu[startIndex], this.mainMenu[mainIndex]];
+  },
+};
+
+// const game = {
+//   team1: 'Bayern Munich',
+//   team2: 'Borrussia Dortmund',
+//   players: [
+//     [
+//       'Neuer',
+//       'Pavard',
+//       'Martinez',
+//       'Alaba',
+//       'Davies',
+//       'Kimmich',
+//       'Goretzka',
+//       'Coman',
+//       'Muller',
+//       'Gnarby',
+//       'Lewandowski',
+//     ],
+//     [
+//       'Burki',
+//       'Schulz',
+//       'Hummels',
+//       'Akanji',
+//       'Hakimi',
+//       'Weigl',
+//       'Witsel',
+//       'Hazard',
+//       'Brandt',
+//       'Sancho',
+//       'Gotze',
+//     ],
+//   ],
+//   score: '4:0',
+//   scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+//   date: 'Nov 9th, 2037',
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+// };
+
+// const gameEvents = new Map([
+//   [17, '⚽️ GOAL'],
+//   [36, '🔁 Substitution'],
+//   [47, '⚽️ GOAL'],
+//   [61, '🔁 Substitution'],
+//   [64, '🔶 Yellow card'],
+//   [69, '🔴 Red card'],
+//   [70, '🔁 Substitution'],
+//   [72, '🔁 Substitution'],
+//   [76, '⚽️ GOAL'],
+//   [80, '⚽️ GOAL'],
+//   [92, '🔶 Yellow card'],
+// ]);
+
+// //1
+// const events = new Set(gameEvents.values());
+// console.log(events);
+
+// //2
+// gameEvents.delete(64);
+// console.log(gameEvents);
+
+// //3
+// const time = [...gameEvents.keys()].pop();
+// console.log(`An event happened, on average, every ${time/gameEvents.size} minutes`);
+
+// //4
+// for (const [key, values] of gameEvents){
+
+//   const half = key <= 45 ? 'First' : 'Second';
+//   console.log(`[${half} Half] ${key}: ${values}`)
+
+//   //My Solution
+//   // if (key <= 45 && key > 0){
+//   //   console.log(`[First Half] ${key}: ${values}`);
+//   // }
+//   // else if (key > 45 && key < 90){
+//   //   console.log(`[Second Half] ${key}: ${values}`);
+//   // } else if(key >90){
+//   //   console.log(`[Overtime] ${key}: ${values}`);
+//   // }
+// }
+
+const airline = 'TAP Air Portugal';
+const plane = 'A320';
+
+console.log(airline.toLowerCase());
+console.log(airline.toUpperCase());
+
+const passenger = 'jOnAS';
+const passengerLower = passenger.toLowerCase();
+const passengerCorrect = passengerLower[0].toUpperCase()+ passengerLower.slice(1);
+console.log(passengerCorrect);
+
+const correctPunctuation = function(string){
+  const inputLower = string.toLowerCase();
+  const outputCorrect = inputLower[0].toUpperCase()+inputLower.slice(1);
+  console.log(outputCorrect);
+  return outputCorrect;
+}
+
+correctPunctuation('jOnAS');
+
+const email = 'hello@jonas.io';
+const loginEmail = '  Hello@Jonas.Io \n';
+
+const lowerEmail = loginEmail.toLowerCase();
+const trimmedEmail = lowerEmail.trim();
+console.log(trimmedEmail);
+
+const normalizedEmail = loginEmail.toLowerCase().trim();
+console.log(normalizedEmail);
+console.log(email === normalizedEmail);
+
+const priceGB = '288,97';
