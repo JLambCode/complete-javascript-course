82e650131444149ccb12adcb9046e162a7eedde6
JLambCode <66347648+SaveJL101@users.noreply.github.com>
2021-02-05 17:14:43 -0500

    Update script.js

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -74,3 +74,23 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+let arr = ['a','b','c', 'd','e'];
+
+console.log(arr.splice(2));
+
+for (const movement of movements){
+  if(movement > 0){
+    console.log(`You deposited ${movement}`);
+  } else {
+    console.log(`You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+movements.forEach(function(movement){
+  if(movement > 0){
+    console.log(`You deposited ${movement}`);
+  } else {
+    console.log(`You withdrew ${Math.abs(movement)}`);
+  }
+})
