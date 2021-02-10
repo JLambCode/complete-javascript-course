095b2c407e2c6bd8437573380e31545ea070d64e
SaveJL101 <66347648+SaveJL101@users.noreply.github.com>
2021-02-09 20:46:42 -0500

    Update script.js

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -82,6 +82,19 @@ const displayMovements = function (movements) {
 
 displayMovements(account1.movements);
 
+const createUsername = function (accts) {
+  accts.forEach(function (accts) {
+    user
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+
+  return username;
+};
+createUsername(accounts);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -152,3 +165,22 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 // checkDogs(dogsJulia1, dogsKate1);
 // checkDogs(dogsJulia2, dogsKate2);
+
+// const eurToUsd = 1.1;
+// const movementsUSD = movements.map(function (mov) {
+//   return mov * eurToUsd;
+// });
+
+// console.log(movements);
+// console.log(movementsUSD);
+
+// const movToUsdArrow = movements.map(mov => mov * eurToUsd);
+
+// const movementsDescriptions = movements.map(
+//   (mov, i) =>
+//     `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
+//       mov
+//     )}`
+// );
+// console.log(movementsDescriptions);
+// console.log(movements);
